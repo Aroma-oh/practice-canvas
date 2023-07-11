bd77f7b40c04f8fa5d3a43ed6201f22e89868916
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-12 02:00:23 +0900

    feat: 선그리기 이벤트

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d"); // context === painting brush
+const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
 
+ctx.lineWidth = 2;
+ctx.moveTo(0, 0);
 
+function onClick(event) {
+  console.log(event);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
