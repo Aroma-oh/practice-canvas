24398d2342bda2a83630ad9b4784457b87a619a3
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-12 01:42:11 +0900

    feat: 집 그리기

--- src/app.js
+++ src/app.js
@@ -4,21 +4,11 @@ const ctx = canvas.getContext("2d"); // context === painting brush
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 200); // 사각형 그리기 
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
-ctx.fillRect(200, 50, 100, 200);
-ctx.strokeRect(350, 50, 100, 200);
-
-ctx.beginPath(); // 경로 끊기: 이전 경로와 단절
-ctx.rect(50, 300, 100, 200);
-setTimeout(() => {
-  ctx.fillStyle = "green";
-  ctx.fill();
-}, 1000);
-
-ctx.moveTo(50, 550); // 1. 좌표 이동
-ctx.lineTo(150, 550); // 2. 해당 좌표에서 선긋기
-ctx.lineTo(150, 750);
-ctx.lineTo(50, 750);
-ctx.lineTo(50, 550);
-ctx.stroke();
