978fc9f51ba14054c202c90d1efcc1daf5b91959
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-12 16:35:53 +0900

    feat: color option

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const usedColor = Array.from(document.getElementsByClassName("used-color"));
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
@@ -7,6 +9,7 @@ canvas.height = 800;
 
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+const usedColors = [];
 
 function onMove(event) {
   if (isPainting) {
@@ -30,9 +33,23 @@ function changeLineWidth(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function changeColor(event) {
+  ctx.strokeStyle = event.target.value;
+  usedColors.unshift(event.target.value);
+}
+
+function clickChangeColor(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  color.value = colorValue;
+  usedColors.unshift(colorValue);
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting)
 
 lineWidth.addEventListener("change", changeLineWidth);
+color.addEventListener("change", changeColor);
+usedColor.forEach(color => color.addEventListener("click", clickChangeColor));
