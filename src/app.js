58fbe580725dad883d6dc2bb657a3f981188011d
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-07-12 21:36:48 +0900

    feat: 지우개 기능 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
+const eraser = document.getElementById("eraser");
+const brush = document.getElementById("brush");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const ctx = canvas.getContext("2d");
 
@@ -8,22 +10,35 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = lineWidth.value;
+
+const MODE_BUTTON = ["brush", "erase"];
+let mode = "brush";
 let isPainting = false;
 
 function onMove(event) {
-  if (isPainting) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (mode === "brush") {
+    if (isPainting) {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+    ctx.moveTo(x, y);
     return;
   }
-  ctx.moveTo(event.offsetX, event.offsetY);
+  if (mode === "erase") {
+    if (isPainting) {
+      ctx.clearRect(x - ctx.lineWidth / 2, y - ctx.lineWidth / 2, ctx.lineWidth, ctx.lineWidth);
+    }
+  }
 }
 
 function startPainting() {
   isPainting = true;
 }
 
-function cancelPainting() {
+function stopPainting() {
   isPainting = false;
   ctx.beginPath();
 }
@@ -34,21 +49,33 @@ function changeLineWidth(event) {
 
 function changeColor(event) {
   ctx.strokeStyle = event.target.value;
-  colorOptions.unshift(event.target.value);
 }
 
 function clickChangeColor(event) {
   const colorValue = event.target.dataset.color;
   ctx.strokeStyle = colorValue;
   color.value = colorValue;
-  colorOptions.unshift(colorValue);
+  mode = "brush";
 }
+
+function onEraser() {
+  mode = "erase";
+};
+
+function onBrush() {
+  mode = "brush";
+};
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting)
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting)
 
 lineWidth.addEventListener("change", changeLineWidth);
 color.addEventListener("change", changeColor);
 colorOption.forEach(color => color.addEventListener("click", clickChangeColor));
+
+eraser.addEventListener("click", onEraser);
+brush.addEventListener("click", onBrush);
+
+
